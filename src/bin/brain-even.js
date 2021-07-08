a665e6f08175784be0ca4ce0443a8300847ebc20
Дима Бекешев <bekeshevdimasik@gmai.com>
2021-07-08 11:02:41 +0300

    работай

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -4,5 +4,5 @@ import { greeting, games } from './brain-games.js';
 export const howToPlay = 'Answer "yes" if number even otherwise answer "no".';
 export const questionGames = [[15, 'no'], [6, 'yes'], [7, 'no']];
 
-greeting();
-games();
+greeting;
+games;
