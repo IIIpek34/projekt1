52edaf90f997a21288a4ab808299f001af470bad
Дима Бекешев <bekeshevdimasik@gmai.com>
2021-07-08 09:03:33 +0300

    после экспорта логики brain-games.js

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import { greeting, games } from './brain-games.js';
+
 export const howToPlay = 'Answer "yes" if number even otherwise answer "no".';
 export const questionGames = [[15, 'no'], [6, 'yes'], [7, 'no']];
+
+greeting();
+games();
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,24 +2,29 @@
 import { haveName, haveAnswer } from '../index';
 import { howToPlay, questionGames } from './brain-even';
 
-/* eslint-disable no-console */
-console.log('Welcome to the Brain Games!');
-console.log(howToPlay);
-const name = haveName();
-console.log(`Hello, ${name}`);
-
-for (let i = 0; i < 3; i += 1) {
+let name = '';
+export const greeting = () => {
   /* eslint-disable no-console */
-  console.log(`Question: ${questionGames[0 + i][0]}`);
-  const answer = haveAnswer();
-  if (answer === questionGames[0 + i][1]) {
-    console.log('Correct!');
-    if (i === 2) {
-      console.log(`Congratulations, ${name}!`);
+  console.log('Welcome to the Brain Games!');
+  console.log(howToPlay);
+  name = haveName();
+  console.log(`Hello, ${name}`);
+};
+
+export const games = () => {
+  for (let i = 0; i < 3; i += 1) {
+    /* eslint-disable no-console */
+    console.log(`Question: ${questionGames[0 + i][0]}`);
+    const answer = haveAnswer();
+    if (answer === questionGames[0 + i][1]) {
+      console.log('Correct!');
+      if (i === 2) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${questionGames[0 + i][1]}'`);
+      console.log(`Let\'s try again, ${name}!`);
+      break;
     }
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${questionGames[0 + i][1]}'`);
-    console.log(`Let\'s try again, ${name}!`);
-    break;
   }
-}
+};
